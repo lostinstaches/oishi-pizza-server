55c4f0fa0b68c50cb9e6cea0c58999d49c2d8012
Igor Katselenbogen <underwoodkats@gmail.com>
2020-01-13 19:57:35 +0900

    init commit

---
+++ src/main/java/com/underwoodkats/oishipizza/OishiPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.underwoodkats.oishipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OishiPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OishiPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/underwoodkats/oishipizza/OishiPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.underwoodkats.oishipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OishiPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
