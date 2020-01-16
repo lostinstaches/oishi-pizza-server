8d9a173705e177e41244a58c4915c5d1978346af
Igor Katselenbogen <underwoodkats@gmail.com>
2020-01-16 22:16:42 +0900

    Connected entity item with order as many to many connection

--- src/main/java/com/underwoodkats/oishipizza/models/Item.java
+++ src/main/java/com/underwoodkats/oishipizza/models/Item.java
@@ -1,13 +1,15 @@
 package com.underwoodkats.oishipizza.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Set;
 
-
-@Entity
 @Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "orderItems"})
 public class Item {
 
     @Id
@@ -15,17 +17,22 @@ public class Item {
     private Integer id;
 
     @NotNull
+    @Column(name = "title")
     private String title;
 
     @NotNull
+    @Column(name = "priceDollar")
     private Double priceDollar;
 
     @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(name = "type")
     private Item.Type type;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "imagePath")
     private String imagePath;
 
     public enum Type {
@@ -33,5 +40,23 @@ public class Item {
         BEVERAGE,
         DESSERT,
         OTHER
+    }
+
+    @OneToMany(
+            mappedBy = "item",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true)
+    private Set<OrderItem> recipeIngredients;
+
+    public Item(String title, Double priceDollar, Item.Type type, String description, String imagePath) {
+        this.title = title;
+        this.priceDollar = priceDollar;
+        this.type = type;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+
+    public Item() {
     }
 }
